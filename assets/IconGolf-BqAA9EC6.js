7d4412e2551b0f635d6ef00169ebeb12615b89b6
bjs0026 <bjs0026@gmail.com>
2026-06-01 17:57:39 -0400

    Delete assets directory

--- assets/IconGolf-BqAA9EC6.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./index-Chlpzzl-.js";var t=e(`outline`,`golf`,`Golf`,[[`path`,{d:`M12 18v-15l7 4l-7 4`,key:`svg-0`}],[`path`,{d:`M9 17.67c-.62 .36 -1 .82 -1 1.33c0 1.1 1.8 2 4 2s4 -.9 4 -2c0 -.5 -.38 -.97 -1 -1.33`,key:`svg-1`}]]);export{t};

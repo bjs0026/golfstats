ae422c18e8c61a358569ec610d96dfd85cfb7ead
bjs0026 <bjs0026@gmail.com>
2026-07-21 10:22:01 -0400

    Delete assets directory

--- assets/TextInput-yMYJfvQ3.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./InputBase-CYNqJPao.js";import{N as t,R as n,ft as r,wt as i,xt as a}from"./index-Zy1KC3jg.js";var o=i(r(),1);a();var s=t((t,r)=>(0,o.jsx)(e,{component:`input`,ref:r,...n(`TextInput`,null,t),__staticSelector:`TextInput`}));s.classes=e.classes,s.displayName=`@mantine/core/TextInput`;export{s as t};
